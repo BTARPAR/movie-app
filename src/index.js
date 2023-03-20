69cfbe77ee0bc25ada58b3633284a2d74cbd0959
Bhargav Tarpara <tarpara_bhargav@yahoo.in>
2023-03-19 17:33:59 -0700

    remove homepage from package.json

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import { DetailedView, SearchResults } from './pages';
 import reportWebVitals from './reportWebVitals';
 
@@ -11,8 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <Routes>
-      <Route path="/" element={<SearchResults />} />
-      {/*<Route path="/" element={<App />} />*/}
+      <Route path="/" element={<App />} />
       <Route path="/search" element={<SearchResults />} />
       <Route path="/title" element={<DetailedView />} />
     </Routes>
